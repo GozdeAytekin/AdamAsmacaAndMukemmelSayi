1adc2926aa84eeb2a408a6e1299e2b0bbee8190c
Neos Ankara 2022-003 <Neos Ankara 2022-003@NEOSAKADEMIANKA>
2023-02-28 04:44:10 +0300

    Add project files.

---
+++ AdamAsmacaOyunu/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmacaOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        List<string> Kelimeler = new List<string> { "MANİSA", "MALATYA", "KASTAMONU", "KAYSERİ", "KIRŞEHİR", "İZMİR", "ANKARA", "İSTANBUL" };
+        string secilen;
+        int sayac = 0;
+        int hak = 6;
+        int harf;
+        Random rnd= new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> Kelimeler = new List<string> { "MANİSA", "MALATYA", "KASTAMONU","KAYSERİ","KIRŞEHİR","İZMİR","ANKARA","İSTANBUL" };
+            
+            pictureBox1.Load("Gorseller/aa" + sayac + ".png");
+            secilen=Kelimeler[rnd.Next(Kelimeler.Count)];
+            label5.Text= secilen;
+            harf = secilen.Length;
+            label6.Text =Convert.ToString(harf);
+            label7.Text = Convert.ToString(hak);
+            for (int i=0;i<secilen.Length;i++)
+            {
+                label4.Text += "_ ";
+            }
+             
+        }
+
+
+        private void HarfSec(object sender, EventArgs e)
+        {
+            Button seciliBtn = sender as Button;
+           
+            if (secilen.Contains(seciliBtn.Text) == false)
+            {
+                sayac++;
+                hak--;
+                label7.Text = Convert.ToString(hak);
+                pictureBox1.Load("Gorseller/aa" + sayac + ".png");
+            }
+           
+            else
+            {
+                string text = label4.Text.Replace(" ", "");
+                for (int i = 0; i < secilen.Length; i++)
+                {
+                    if (secilen[i].ToString() == seciliBtn.Text)
+                    {
+
+                        text = ReplaceAt(text, i, 1, seciliBtn.Text);
+
+                    }
+                }
+                string sonuc = "";
+                for (int i = 0; i < text.Length; i++)
+                {
+                    sonuc += text[i].ToString() + " ";
+                    label4.Text = sonuc;
+                   
+                }
+            }
+            if (hak == 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("KAYBETTİNİZ... Kelime : " + secilen + " Yeniden Oynamak İster Misiniz?", "Adam Asmaca ", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                    Application.Restart();
+                else
+                    Application.Exit();
+            }
+            if (label4.Text.Replace(" ", "") == secilen)
+            {
+
+                DialogResult dialogResult = MessageBox.Show("KAZANDINIZ...  Kelime : " + secilen + " Yeniden Oynamak İster Misiniz?", "Adam Asmaca ", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                    Application.Restart();
+                else
+                    Application.Exit();
+            }
+
+        }
+
+        public string ReplaceAt(string str, int index, int length, string replace)
+        {
+            return str.Remove(index, Math.Min(length, str.Length - index)).Insert(index, replace);
+        }
+
+
+        
+    }
+}
---
+++ MukemmelSayi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MukemmelSayi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kontrol etmek istediginiz sayiyi girin");
+            
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            if (sayi== toplam )
+            {
+                Console.WriteLine("Girilen " + sayi + " sayisi Mukemmel sayidir...");
+            }
+            else
+            {
+                Console.WriteLine("Girilen "+sayi+" sayisi Mukemmel sayi degildir...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+/*
+    1.Framework, uygulama çatısı olarak çevrilebilmektedir. Yani biz örneğin bir web projesi geliştirirken .Net’in bu durum için geliştirdiği frameworku kullanarak bu işi daha kolay ve daha hızlı geliştiririz. Aynı şekilde .Net core da bir .Net Fw gibi bir çerçeve, bir uygulama çatısıdır. .Net dünyasının trend fw’u  son zamanlarda iş ilanlarında olsun eğitim içeriklerinde olsun daha sık görmeye başladığım .net core diye düşünüyorum. 
+    2.Açıkçası daha önce böyle bir normalizasyon gerektirecek bir işle uğraşmadım. 1NF, 2NF.. gibi şeyler duydum ama sadece duydum :/ 
+    3.C#, Asp.net, html, js kullanarak web programlamasını yapar, verileri Mssql’de tutarım. 
+    4.Udemy, Btk akademi, Youtube gibi platformları kullanırım. Engin demiroğ, murat yücedağ, şadi evren şeker, sadık turan gibi kişileri takip ediyor, çeşitli platformlarda eğitimlerine katılıyorum. 
+    5.Bilgisayarda pek fazla oyun oynamam, cities: skylines oynuyorum ara sıra, daha çok arkadaşlarımla mobil oyunlar oynarım, pubg mobile gibi. 
+
+ */
